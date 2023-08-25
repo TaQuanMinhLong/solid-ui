1aa593eb564671d8abee9030ce3f4bffc97656a8
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-25 19:07:29 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <main class="bg-input py-16 min-h-screen font-sans text-slate-800">
-      <div></div>
+    <main class="bg-white flex items-center justify-center min-h-screen text-slate-800">
+      <div class="flex flex-col gap-3">\ </div>
     </main>
   );
 }
