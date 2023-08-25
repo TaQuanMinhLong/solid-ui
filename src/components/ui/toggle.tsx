b98e50a4e91c2b9db509a1b6580bd7500dbc1919
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-25 19:07:08 +0700

    add toggle component

---
+++ src/components/ui/toggle.tsx
@@ -0,0 +1,39 @@
+import type { Accessor, ComponentProps, Setter } from "solid-js";
+import { createUncontrolledSignal } from "~/hooks";
+import { splitProps } from "solid-js";
+import { cn } from "~/lib/styles";
+
+interface ToggleProps extends Omit<ComponentProps<"button">, "onClick"> {
+  onChange?: Setter<boolean>;
+  checked?: Accessor<boolean>;
+}
+
+export function Toggle(props: ToggleProps) {
+  const [{ checked, onChange }, others] = splitProps(props, ["class", "checked", "onChange"]);
+  const [isChecked, setIsChecked] = createUncontrolledSignal({
+    finalValue: false,
+    getter: checked,
+    setter: onChange,
+  });
+
+  return (
+    <button
+      data-state={isChecked() ? "checked" : "unchecked"}
+      aria-checked={isChecked()}
+      class={cn(
+        "peer inline-flex h-6 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+        props.class
+      )}
+      onClick={() => setIsChecked(!isChecked())}
+      role="switch"
+      {...others}
+    >
+      <span
+        data-state={isChecked() ? "checked" : "unchecked"}
+        class={cn(
+          "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
+        )}
+      />
+    </button>
+  );
+}
