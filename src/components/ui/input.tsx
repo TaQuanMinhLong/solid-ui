8ce4f1d75d012412491b3f11ddea8b2be87225f2
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-26 22:34:48 +0700

    fix input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,17 +2,14 @@ import type { ComponentProps } from "solid-js";
 import { splitProps } from "solid-js";
 import { cn } from "~/lib/styles";
 
-interface InputProps extends ComponentProps<"input"> {
-  isError?: boolean;
-}
+interface InputProps extends ComponentProps<"input"> {}
 
 export function Input(props: InputProps) {
-  const [{ isError }, others] = splitProps(props, ["class", "isError"]);
+  const [, others] = splitProps(props, ["class"]);
   return (
     <input
       class={cn(
-        "flex h-10 w-full text-slate-900 rounded-md border border-slate-800 bg-white px-3 py-2 text-sm  file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ring-offset-slate-950 focus-visible:ring-white",
-        { "text-red-500 border-red-500 ring-offset-red-500": isError },
+        "flex h-10 w-full invalid:text-destructive invalid:border-destructive rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 invalid:focus-visible:ring-destructive focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         props.class
       )}
       {...others}
