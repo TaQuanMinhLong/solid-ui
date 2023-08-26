5313c75cdf3eef3da4a01244ba849b94bceabdd4
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-26 18:11:01 +0700

    move slots to async only

--- src/components/ui/async.tsx
+++ src/components/ui/async.tsx
@@ -1,5 +1,41 @@
-import { Component, createResource, ErrorBoundary, JSXElement, Show, Suspense } from "solid-js";
-import { getSlots, Slot } from "./slots";
+import type { Component, JSXElement } from "solid-js";
+
+import {
+  createResource,
+  createComputed,
+  ErrorBoundary,
+  Suspense,
+  children,
+  Show,
+  on,
+} from "solid-js";
+import { createStore } from "solid-js/store";
+
+export const getSlots = (_children: JSXElement) => {
+  const parts = children(() => _children);
+  const [slots, setSlots] = createStore<Record<string, JSXElement>>({});
+  createComputed(
+    on(parts, () => {
+      for (const part of parts.toArray() as unknown as SlotProps[]) {
+        if (!part.name) {
+          setSlots("default", () => part as unknown as JSXElement);
+          continue;
+        }
+        setSlots(part.name, () => part.children);
+      }
+    })
+  );
+  return slots;
+};
+
+interface SlotProps {
+  name: string;
+  children: JSXElement;
+}
+
+export const Slot: Component<SlotProps> = (props) => {
+  return props as unknown as JSXElement;
+};
 
 interface AsyncProps<T> {
   promise: Promise<T>;
