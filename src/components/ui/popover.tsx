715ea58c18d986c7cd74f721f551a99dcf08d4e8
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-27 20:46:48 +0700

    add popover component

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,13 +1,16 @@
 import type { ComponentProps } from "solid-js";
+import type { Placement } from "@floating-ui/dom";
 import {
   createUniqueId,
   createContext,
+  createEffect,
   splitProps,
   useContext,
-  onMount,
-  createSignal,
+  onCleanup,
 } from "solid-js";
+import { autoUpdate, computePosition, flip, offset, shift } from "@floating-ui/dom";
 import { createStore } from "solid-js/store";
+import { createSignal } from "solid-js";
 import { cn } from "~/lib/styles";
 
 type PopoverState = "open" | "closed";
@@ -23,36 +26,50 @@ function usePopover() {
 type CreatePopoverOptions = {
   id?: string;
   offset?: number;
+  placement?: Placement;
 };
 
 type PopoverStore = {
   id: string;
   offset: number;
   state: PopoverState;
-  triggerRect?: DOMRect;
+  placement: Placement;
+  triggerRef: HTMLButtonElement | undefined;
+  contentRef: HTMLDivElement | undefined;
 };
 
 /**
  * Create a popover context
  *
- * @default id = createUniqueId(), offset = 8
+ * @default id = createUniqueId(), offset = 8, placement = "bottom-start"
  */
-function createPopover({ id = createUniqueId(), offset = 8 }: CreatePopoverOptions = {}) {
-  const [store, setStore] = createStore<PopoverStore>({ id, offset, state: "closed" });
-
-  const [contentRef, setContentRef] = createSignal<HTMLDivElement>();
+function createPopover({
+  placement = "bottom-start",
+  id = createUniqueId(),
+  offset = 8,
+}: CreatePopoverOptions = {}) {
+  const [store, setStore] = createStore<PopoverStore>({
+    id,
+    offset,
+    placement,
+    state: "closed",
+    contentRef: undefined,
+    triggerRef: undefined,
+  });
 
   return {
-    setContentRef,
-    show: () => contentRef()?.showPopover(),
-    toggle: () => contentRef()?.togglePopover(),
-    hide: () => contentRef()?.hidePopover(),
-    offset: () => store.offset,
+    _offset: () => store.offset,
+    placement: () => store.placement,
+    show: () => store.contentRef?.showPopover(),
+    hide: () => store.contentRef?.hidePopover(),
+    toggle: () => store.contentRef?.togglePopover(),
     popoverId: () => store.id,
     popoverState: () => store.state,
-    setPopoverState: (state: PopoverState) => setStore("state", state),
-    triggerRect: () => store.triggerRect,
-    setTriggerRect: (triggerRect: DOMRect) => setStore("triggerRect", triggerRect),
+    contentRef: () => store.contentRef,
+    triggerRef: () => store.triggerRef,
+    handleToggle: (e: ToggleEvent) => setStore("state", e.newState),
+    setContentRef: (ref: HTMLDivElement | undefined) => setStore("contentRef", ref),
+    setTriggerRef: (ref: HTMLButtonElement | undefined) => setStore("triggerRef", ref),
   };
 }
 
@@ -73,10 +90,7 @@ function Root(props: PopoverRootProps) {
 
 function Trigger(props: ComponentProps<"button">) {
   const [, others] = splitProps(props, ["class", "popovertarget", "popovertargetaction", "ref"]);
-  const { popoverId, setTriggerRect } = usePopover();
-  const [triggerRef, setTriggerRef] = createSignal<HTMLButtonElement>();
-
-  onMount(() => setTriggerRect(triggerRef()!.getBoundingClientRect()));
+  const { popoverId, setTriggerRef } = usePopover();
 
   return (
     <button
@@ -95,34 +109,50 @@ function Trigger(props: ComponentProps<"button">) {
  * Set offset by using transform: translate
  */
 function Content(props: ComponentProps<"div">) {
-  const [, others] = splitProps(props, ["class"]);
-  const { popoverId, popoverState, setPopoverState, triggerRect, setContentRef, offset } =
-    usePopover();
-  const handleToggle = (e: ToggleEvent) => setPopoverState(e.newState);
+  const {
+    _offset,
+    popoverId,
+    placement,
+    contentRef,
+    triggerRef,
+    handleToggle,
+    popoverState,
+    setContentRef,
+  } = usePopover();
+
+  const [position, setPosition] = createSignal({ x: 0, y: 0 });
+
+  createEffect(() => {
+    if (contentRef() && triggerRef()) {
+      const cleanUp = autoUpdate(triggerRef()!, contentRef()!, async function () {
+        const { x, y } = await computePosition(triggerRef()!, contentRef()!, {
+          middleware: [flip(), offset(_offset()), shift()],
+          placement: placement(),
+        });
+        setPosition({ x, y });
+      });
+      onCleanup(() => cleanUp());
+    }
+  });
+
   return (
     <div
       ref={setContentRef}
       role="dialog"
       tabIndex={-1}
-      class={cn(
-        "absolute m-0 transition-[opacity,transform] duration-200 data-[state=open]:translate-y-0 data-[state=closed]:-translate-y-1 data-[state=open]:opacity-100 data-[state=closed]:opacity-0 data-[state=open]:scale-100 data-[state=closed]:scale-100 bg-input",
-        props.class
-      )}
-      style={
-        popoverState() === "open"
-          ? {
-              top: `${triggerRect()!.y + triggerRect()!.height + offset()}px`,
-              left: `${triggerRect()!.x}px`,
-            }
-          : undefined
-      }
+      class="absolute m-0 p-0 transition-[opacity,transform] duration-200 data-[state=open]:translate-y-0 data-[state=closed]:-translate-y-1 data-[state=open]:scale-100 data-[state=closed]:scale-100"
+      style={{
+        left: `${position().x}px`,
+        top: `${position().y}px`,
+      }}
       data-state={popoverState()}
       onToggle={handleToggle}
       id={popoverId()}
       popover
-      {...others}
     >
-      {props.children}
+      <div {...props} data-state={popoverState()}>
+        {props.children}
+      </div>
     </div>
   );
 }
